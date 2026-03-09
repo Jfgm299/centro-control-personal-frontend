cfe88613a2203cea5f614388a43773c16c023430
jf <jfgm299@gmail.com>
2026-03-10 00:20:53 +0100

    fix(auth): Fix translation in login tab

--- src/components/layout/AppShellDesktop.jsx
+++ src/components/layout/AppShellDesktop.jsx
@@ -31,7 +31,7 @@ export default function AppShellDesktop() {
     <>
       <DotBackground />
       {!user && <LoginPopup />}
-      <UserMenu />
+      {user && <UserMenu />}
       <TabBar />
       <ModuleContainer>
         {modulesLoaded ? (
